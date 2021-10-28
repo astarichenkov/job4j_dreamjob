838a8438463c3ead33cc87b4839f9e27442df54e
Anton Starichenkov <anton.starichenkov@gmail.com>
2021-10-29 00:41:48 +0300

    checkstyle

--- src/main/java/ru/job4j/dream/utils/FileUtils.java
+++ src/main/java/ru/job4j/dream/utils/FileUtils.java
@@ -6,8 +6,8 @@ public class FileUtils {
     public static File getFileById(String id) {
         File targetFile = null;
         for (File file : new File("c:\\images\\").listFiles()) {
-            if ((id + ".jpg").equalsIgnoreCase(file.getName()) ||
-                    (id + ".png").equalsIgnoreCase(file.getName())) {
+            if ((id + ".jpg").equalsIgnoreCase(file.getName())
+                    || (id + ".png").equalsIgnoreCase(file.getName())) {
                 targetFile = file;
                 break;
             }
